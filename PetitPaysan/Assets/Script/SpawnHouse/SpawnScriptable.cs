9afdddd8e59307cfc3e794ef8bc99bd601957079
Antonin <nerostormal@gmail.com>
2021-04-11 18:26:55 +0200

    Initial Files

---
+++ PetitPaysan/Assets/Script/SpawnHouse/SpawnScriptable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "spawnScriptable", menuName = "ScriptableObjects/spawnScriptable", order = 1)]
+public class SpawnScriptable : ScriptableObject
+
+{
+    public int troopInit;
+    public int troopMax;
+    public whosSpawn who;
+}
+
+
+public enum whosSpawn
+{
+    neutral = 0,
+    player = 1,
+    ennemy = 2,
+}
