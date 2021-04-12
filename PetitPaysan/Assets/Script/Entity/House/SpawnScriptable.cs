0c16d19d1362aa09a83861e03920952acacfe6f2
Joachim Miens <contact@joachim-miens.com>
2021-04-12 04:14:07 +0200

    Initials Files

---
+++ PetitPaysan/Assets/Script/Entity/House/SpawnScriptable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "spawnScriptable", menuName = "ScriptableObjects/spawnScriptable", order = 1)]
+public class SpawnScriptable : ScriptableObject
+{
+    public int troopInit;
+    public int troopMax;
+    public Owner who;
+}
+
+
+public enum Owner
+{
+    Unknown = -1,
+    Neutral = 0,
+    Player = 1,
+    Enemy = 2,
+}
---
+++ PetitPaysan/Assets/Script/Entity/Troop/Troop.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Troop : MonoBehaviour
+{
+
+    public Owner currentOwner = Owner.Unknown;
+    public TownManager TargetTown { get; set; } = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(TargetTown != null)
+        {
+            transform.position = Vector2.MoveTowards(this.transform.position, TargetTown.transform.position, Time.deltaTime * 50);
+        }
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Debug.Log($"Touched { collision.gameObject.name }");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Debug.Log($"Trigger { collision.gameObject.name }");
+
+        if(collision.gameObject == TargetTown.gameObject)
+        {
+            //Debug.Log($"Bison: YES");
+
+            TargetTown.TroopAttack(this);
+
+            Destroy(gameObject);
+
+        }
+    }
+
+}
